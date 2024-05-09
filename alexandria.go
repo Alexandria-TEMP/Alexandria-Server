b46c765001b2cf752a30435eb2f2f4160355cefe
Maxime Caux <j.m.caux@student.tudelft.nl>
2024-05-09 14:34:28 +0200

    make minor code quality changes

--- alexandria.go
+++ alexandria.go
@@ -8,15 +8,15 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-	err := r.Run(":8080") // listen and serve on 0.0.0.0:8080
+	err := router.Run(":8080") // listen and serve on 0.0.0.0:8080
 	if err != nil {
-		log.Fatalf("impossible to start server: %s", err)
+		log.Fatalf("unable to start server: %s", err)
 	}
 }
