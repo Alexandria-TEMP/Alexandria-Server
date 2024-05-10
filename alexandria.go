2a7dd1642bb8e5ab749cadb0754ebe49d4825915
Maxime Caux <j.m.caux@student.tudelft.nl>
2024-05-10 13:40:33 +0200

    connect to database with env var credentials

--- alexandria.go
+++ alexandria.go
@@ -5,9 +5,22 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gitlab.ewi.tudelft.nl/cse2000-software-project/2023-2024/cluster-v/17b/alexandria-backend/database"
 )
 
 func main() {
+	// If DB connection fails, terminate
+	_, err := database.ConnectToDatabase()
+	if err != nil {
+		log.Fatalf("could not connect to database: %s", err)
+	}
+
+	// This will be used to migrate models, once those are annotated.
+	// err = db.AutoMigrate(?)
+	// if err != nil {
+	// 	log.Fatalf("could not migrate models: %s", err)
+	// }
+
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -15,7 +28,7 @@ func main() {
 		})
 	})
 
-	err := router.Run(":8080") // listen and serve on 0.0.0.0:8080
+	err = router.Run(":8080") // listen and serve on 0.0.0.0:8080
 	if err != nil {
 		log.Fatalf("unable to start server: %s", err)
 	}
