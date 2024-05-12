bc4419ad7ab3ad605fbddf2c072da0e0abffdadb
Eve Smura <E.Smura@student.tudelft.nl>
2024-05-12 23:13:25 +0200

    create interface and service classes

---
+++ services/interfaces/userService_interface.go
@@ -0,0 +1,8 @@
+package interfaces
+
+//run to create the mock
+//go:generate mockgen -source=./userService_interface.go -destination=../../mocks/userService_mock.go
+
+type UserService interface {
+
+}
