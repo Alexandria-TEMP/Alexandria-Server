e1b9f475fa28cf7e0ea2f565d2bb624175485c11
Maxime Caux <j.m.caux@student.tudelft.nl>
2024-05-17 14:02:07 +0200

    add dtos to api doc

--- models/forms/postCreationForm.go
+++ models/forms/postCreationForm.go
@@ -1,10 +1,7 @@
 package forms
 
-import (
-	"gitlab.ewi.tudelft.nl/cse2000-software-project/2023-2024/cluster-v/17b/alexandria-backend/models"
-)
-
 type PostCreationForm struct {
-	Collaborators  []uint
-	CurrentVersion models.Version
+	Collaborators []uint
+	// CurrentVersion models.Version
+	// TODO add fields
 }
--- models/forms/projectPostCreationForm.go
+++ models/forms/projectPostCreationForm.go
@@ -1,12 +1,9 @@
 package forms
 
-import (
-	"gitlab.ewi.tudelft.nl/cse2000-software-project/2023-2024/cluster-v/17b/alexandria-backend/models"
-)
-
 type ProjectPostCreationForm struct {
-	models.Post
-	// models.ProjectMetadata // TODO replace with new data
-	OpenMergeRequests   []models.MergeRequest
-	ClosedMergeRequests []models.ClosedMergeRequest
+	// TODO add fields
+	PostCreationForm PostCreationForm
+	// models.ProjectMetadata
+	// OpenMergeRequests   []models.MergeRequest
+	// ClosedMergeRequests []models.ClosedMergeRequest
 }
