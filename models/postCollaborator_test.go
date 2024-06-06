4d0596f02e1fcd50e42f1a75b18d0aad367bd2f2
Maxime Caux <j.m.caux@student.tudelft.nl>
2024-06-06 14:51:29 +0200

    split collaborator.go and collaborator_test.go

---
+++ models/postCollaborator_test.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"encoding/json"
+	"testing"
+
+	"gorm.io/gorm"
+)
+
+func TestPostCollaboratorJSONMarshaling(t *testing.T) {
+	// This model...
+	model := PostCollaborator{
+		Model:             gorm.Model{ID: 55},
+		Member:            Member{},
+		MemberID:          32,
+		PostID:            87,
+		CollaborationType: Author,
+	}
+
+	// should equal this DTO!
+	targetDTO := PostCollaboratorDTO{
+		ID:                55,
+		MemberID:          32,
+		PostID:            87,
+		CollaborationType: Author,
+	}
+
+	dto := PostCollaboratorDTO{}
+
+	bytes, err := model.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = json.Unmarshal(bytes, &dto)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if dto != targetDTO {
+		t.Fatal("parsed DTO did not equal target DTO")
+	}
+}
